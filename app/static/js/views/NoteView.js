8be2436f6ee56bdde77a9ded83117733c72a8fd6
Alexandra Nadova <alexandranadova@gmail.com>
2020-12-10 17:06:04 +0100

    notes pages

--- app/static/js/views/NoteView.js
+++ app/static/js/views/NoteView.js
@@ -1,16 +1,51 @@
 import AbstractView from "./AbstractView.js";
 
 export default class extends AbstractView {
-    constructor(params) {
-        super(params);
-        this.noteId = params.id;
-        this.setTitle("Viewing Note");
-    }
+  constructor(params) {
+    super(params);
+    this.noteId = params.id;
+    this.setTitle("Viewing Note");
+  }
 
-    async getHtml() {
-        return `
-            <h1>Note</h1>
-            <p>You are viewing note #${this.noteId}.</p>
+  async getJs(url, token) {
+    document.querySelector("#deleteBtn").addEventListener("click", () => {
+      let userId = localStorage.getItem("id");
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          window.location.href = "/notes";
+        }
+      };
+      xhttp.open("DELETE", url + "/notes/" + userId + "/" + this.noteId, true);
+      xhttp.setRequestHeader("x-access-token", token);
+      xhttp.send();
+    });
+  }
+
+  async getHtml(url, token, res) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", url + "/notes/1/" + this.noteId, true);
+    xhttp.setRequestHeader("x-access-token", token);
+    xhttp.send();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        var noteInfo = JSON.parse(this.responseText);
+        let html = `
+          <h1>Note</h1>
+          <p>You are viewing note #${noteInfo.id}.</p>
+          <button id="deleteBtn">Delete note</button>
+          <div>
+            <ul>
+              <li>ID: ${noteInfo.id}</li>
+              <li>Title: ${noteInfo.title}</li>
+              <li>Content: ${noteInfo.content}</li>
+              <li>Created at: ${noteInfo.createdAt}</li>
+              <li>Updated at: ${noteInfo.updatedAt}</li>
+            </ul>
+          </div>
         `;
-    }
+        if (res) res(html);
+      }
+    };
+  }
 }
--- app/static/js/views/Notes.js
+++ app/static/js/views/Notes.js
@@ -1,15 +1,29 @@
 import AbstractView from "./AbstractView.js";
 
 export default class extends AbstractView {
-    constructor(params) {
-        super(params);
-        this.setTitle("Notes");
-    }
+  constructor(params) {
+    super(params);
+    this.setTitle("Notes");
+  }
 
-    async getHtml() {
-        return `
-            <h1>Notes</h1>
-            <p>You are viewing your notes!</p>
-        `;
-    }
+  getHtml(url, token, res) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", url + "/notes/1", true);
+    xhttp.setRequestHeader("x-access-token", token);
+    xhttp.send();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        var notes = JSON.parse(this.responseText);
+        let html = "<div><ul>";
+        notes.data.forEach((note) => {
+          html += `
+          <li>
+            <a href="./notes/${note.id}">ID:${note.id} title:${note.title}</a>
+          </li>`;
+        });
+        html += "</ul></div>";
+        if (res) res(html);
+      }
+    };
+  }
 }
